b31ad1371325fce518de9df8d5d74cb5ca826b3f
ShyBigBoy <1104449876@qq.com>
2018-01-18 19:22:40 +0800

    update

--- tensorflow/face-detection.py
+++ tensorflow/face-detection.py
@@ -20,7 +20,7 @@ with tf.Graph().as_default():
         with sess.as_default():
             pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
 
-image_path = '/home/cqh/faceData/multi_face/multi_face3.jpg'
+image_path = 'facedata/mmexport1515650182482.jpg'
 
 img = misc.imread(image_path)
 bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
